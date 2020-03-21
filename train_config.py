81a4c49e28ba8f8cb1440f89e927beb563661eee
codesteller <pallab.vsp@gmail.com>
2020-03-21 10:07:36 +0530

    hyperparameter changed

--- train_config.py
+++ train_config.py
@@ -13,8 +13,8 @@ vgg_pretrained = '/home/codesteller/models/pretrained/VGG-16_mod2FCN_ImageNet-Cl
 # Set Training Hyper Parameters
 # NOTE: Learning Rate is sen by function learning_rate_schedule in do_train_vgg16fcn.py
 num_classes = 34  # TODO: Set the number of segmentation classes.
-train_batch_size = 2  # TODO: Set the training batch size.
-val_batch_size = 2  # TODO: Set the validation batch size.
+train_batch_size = 3  # TODO: Set the training batch size.
+val_batch_size = 3  # TODO: Set the validation batch size.
 epochs = 20  # TODO: Set the number of epochs to train for.
 keep_prob = 0.6
 
